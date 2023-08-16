78942a13afb9dd3e8730c1326dca70af6b7bb0e0
Manan Agrawal <agrawalmanan2@gmail.com>
2023-08-17 01:02:27 +0530

    chnages

--- src/components/Pages/Homepage/Card3.jsx
+++ src/components/Pages/Homepage/Card3.jsx
@@ -4,10 +4,7 @@ import video from "./videoMain.mp4"
 const Card3 = () => {
   return (
     <div className='card3'>
-      <video autoPlay loop muted>
-        <source src={video} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      
     </div>
   );
 };
